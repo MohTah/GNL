9cc5ab9edf37a4ec036161a23bff7dd116d3efb2
Tahar <mohandtahar91@gmail.com>
2021-05-06 15:18:33 +0200

    norme ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: msadaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 13:14:27 by msadaoui          #+#    #+#             */
-/*   Updated: 2021/05/06 14:52:03 by msadaoui         ###   ########.fr       */
+/*   Updated: 2021/05/06 15:17:28 by msadaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,17 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stddef.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stddef.h>
 
-int	get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_strdel(char **as);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strdup(const char *s);
 
-#ifndef	BUFFER_SIZE
-# define BUFFER_SIZE 32
-
-#endif
 #endif
